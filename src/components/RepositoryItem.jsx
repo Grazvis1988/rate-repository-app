b2d7bdd7e1a36d0cfa978e65c9dc947618fe0a90
Grazvis <grazvydas.untulis@gmail.com>
2022-01-13 14:03:05 +0200

    ex10.3

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+	item: {
+		padding: 20,
+		marginVertical: 8,
+		marginHorizontal: 16,
+		fontSize: 20
+	}
+});
+
+const RepositoryItem = ({ item }) => (
+	<View style={styles.item}>
+		<Text>Full name: {item.fullName}</Text>
+		<Text>Description: ${item.description}</Text>
+		<Text>Language: ${item.language}</Text>
+		<Text>Stars: ${item.stargazersCount}</Text>
+		<Text>Forks: ${item.forksCount}</Text>
+		<Text>Reviews: ${item.reviewCount}</Text>
+		<Text>Rating: ${item.ratingAverage}</Text>
+	</View>
+);
+
+export default RepositoryItem;
